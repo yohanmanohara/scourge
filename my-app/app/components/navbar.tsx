8409994cdbf543f8bf87c571bfe4d290a83f5e04
yohanmanohara <warushayohan80@gmail.com>
2024-06-10 01:29:46 +0530

    #

--- my-app/app/components/navbar.tsx
+++ my-app/app/components/navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function Navbar() {
   return (
-    <div>
-      hutto meka navbar eka
-    </div>
-  )
+    <nav className="w-full h-20 bg-black flex justify-center items-center">
+      <ul className="flex space-x-4 text-white">
+        <li><img src="/logo3.png" alt="" width={300}/></li>
+      </ul>
+    </nav>
+  );
 }
