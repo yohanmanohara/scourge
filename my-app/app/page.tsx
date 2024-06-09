0ef199384b57ee068693da562a68e93d54eb695f
yohanmanohara <warushayohan80@gmail.com>
2024-06-10 01:56:18 +0530

    #

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import Navbar from "@/app/components/navbar";
-export default function Home() {
-  return (
-    
 
+export default function Home() {
+  const googleDriveFileId = "1OafnTIdOB-bzLZAZZp1aykN0wXSTW8_1";
+  const videoSrc = `https://drive.google.com/uc?export=download&id=${googleDriveFileId}`;
+
+  return (
     <main>
-     
       <Navbar />
-      <video src=""></video>
-     
+      <div className="video-container">
+        <video autoPlay muted loop controls width="100%" height="auto">
+          <source src={videoSrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </main>
   );
 }
