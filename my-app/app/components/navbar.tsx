1a5d7f873513396ec71ed0f424b6d828a447a47a
yohanmanohara <warushayohan80@gmail.com>
2024-06-10 02:43:38 +0530

    #

--- my-app/app/components/navbar.tsx
+++ my-app/app/components/navbar.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-20 bg-black flex justify-center items-center">
+    <div className="w-full h-20 bg-black flex justify-center items-center ">
       <ul className="flex space-x-4 text-white">
         <li><img src="/logo3.png" alt="" width={300}/></li>
       </ul>
-    </nav>
+    </div>
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,18 +1,21 @@
-import Image from "next/image";
 import Navbar from "@/app/components/navbar";
 
 export default function Home() {
-  const googleDriveFileId = "1OafnTIdOB-bzLZAZZp1aykN0wXSTW8_1";
-  const videoSrc = `https://drive.google.com/uc?export=download&id=${googleDriveFileId}`;
+  const googleDriveFileId = "1OafnTIdOB-bzLZAZZp1aykN0wXSTW8_1"; // Replace with your Google Drive file ID
+  const videoSrc = `https://drive.google.com/uc?id=${googleDriveFileId}`;
 
   return (
     <main>
       <Navbar />
       <div className="video-container">
-        <video autoPlay muted loop controls width="100%" height="auto">
-          <source src={videoSrc} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <video
+          src={videoSrc}
+          controls
+          autoPlay
+          loop
+          muted
+          width="100%"
+        />
       </div>
     </main>
   );
