86473af5490d5178e236e85b911994851b1cadf6
yohanmanohara <warushayohan80@gmail.com>
2024-07-13 16:04:09 +0530

    #

--- client/app/components/navbar.tsx
+++ client/app/components/navbar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Image from 'next/image';
 export default function Navbar() {
   return (
     <div className="w-full h-20 bg-black flex justify-center items-center ">
       <ul className="flex space-x-4 text-white">
-        <li><img src="/logo3.png" alt="" width={300}/></li>
+        <li><Image src="/logo3.png" alt="" width={300}/></li>
       </ul>
     </div>
   );
